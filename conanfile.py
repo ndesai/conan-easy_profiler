262e5f928585d7dcbe519e7bd9ce81bf0ef881f2
Niraj Desai <niraj.desai@nio.io>
2019-01-15 11:20:05 -0800

    Updates

--- conanfile.py
+++ conanfile.py
@@ -2,25 +2,24 @@ from conans import ConanFile, CMake, tools
 
 
 class EasyprofilerConan(ConanFile):
-    name = "EasyProfiler"
+    name = "easy_profiler"
     version = "2.0.1"
     license = "https://github.com/yse/easy_profiler/blob/develop/LICENSE"
     author = "https://github.com/yse/easy_profiler/graphs/contributors"
-    url = "<Package recipe repository url here, for issues about the package>"
+    url = "https://githost.nevint.com/skywalker/utilities/conan-easy_profiler"
     description = "Lightweight cross-platform profiler library for c++"
-    topics = ("<Put some tag here>", "<here>", "<and here>")
+    topics = ("easy_profiler", "profiling", "C++", "profiler")
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
-    default_options = "shared=False"
+    default_options = "shared=True"
     generators = "cmake"
 
     # Custom variables
-    source_folder = "easy_profiler"
     source_url = "https://github.com/ndesai/easy_profiler.git"
     source_branch = "feature/qnx-support"
 
     def source(self):
-        self.run("git clone %s %s" % (self.source_url, self.source_folder))
+        self.run("git clone %s %s" % (self.source_url, self.name))
         self.run("cd %s && git checkout %s" % (self.name, self.source_branch))
         # This small hack might be useful to guarantee proper /MT /MD linkage
         # in MSVC if the packaged project doesn't have variables to set it
@@ -32,7 +31,7 @@ class EasyprofilerConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder=self.source_folder)
+        cmake.configure(source_folder=self.name)
         cmake.build()
 
         # Explicit way:
@@ -41,13 +40,13 @@ class EasyprofilerConan(ConanFile):
         # self.run("cmake --build . %s" % cmake.build_config)
 
     def package(self):
-        self.copy("*.h", dst="include", src="hello")
-        self.copy("*hello.lib", dst="lib", keep_path=False)
+        self.copy("*.h", dst="include", src="easy_profiler/easy_profiler_core/include")
+        # self.copy("*hello.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = [self.source_folder]
+        self.cpp_info.libs = [self.name]
 
