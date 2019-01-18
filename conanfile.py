36a504336f6a7c456056c02bca2dba54741b90e9
Niraj Desai <niraj.desai@nio.io>
2019-01-18 14:43:20 -0800

    Copy lib for win32

--- conanfile.py
+++ conanfile.py
@@ -41,7 +41,7 @@ class EasyprofilerConan(ConanFile):
 
     def package(self):
         self.copy("*.h", dst="include", src="easy_profiler/easy_profiler_core/include")
-        # self.copy("*hello.lib", dst="lib", keep_path=False)
+        self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
